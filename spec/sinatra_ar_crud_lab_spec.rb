0721b55baa170ea3fb08ab4ad8e0081749738d93
Alvinluipo <lualvin1234@gmail.com>
2016-01-11 12:42:17 -0500

    Done.

--- spec/sinatra_ar_crud_lab_spec.rb
+++ spec/sinatra_ar_crud_lab_spec.rb
@@ -97,7 +97,8 @@ describe "Blog Post App" do
 
     it "deletes a blog post from the database" do
       visit "/posts/#{@post2.id}"
-      click_button "delete"
+      save_and_open_page
+      click_button 'delete'
       expect(Post.all.count).to eq(1)
       expect(Post.last.name).to eq("Hello World")
     end
