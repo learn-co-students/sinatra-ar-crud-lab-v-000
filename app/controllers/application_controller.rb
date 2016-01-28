2091fad7352c52786dae51919a3bb69392119cf5
c1505 <coreym247@gmail.com>
2016-01-27 21:52:28 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,44 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  patch '/posts/:id' do #update
+    @post = Post.find(params[:id])
+    @post.update(name: params["name"], content: params["content"])
+    redirect "/posts/#{@post.id}"
   end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    @post = Post.create(params)
+    redirect '/posts'
+  end
+
+  get '/posts/:id' do #show
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  delete '/posts/:id/delete' do 
+    # binding.pry
+    Post.find(params[:id]).destroy
+    redirect '/posts'
+  end
+
+
+
+
+
 end
