29134e7386ff03ffcbf9312bce6124674c15d58c
zachnewburgh <zach.newburgh@gmail.com>
2016-02-29 13:31:55 -0500

    blog posting sinatra/activerecord

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,1 +1,17 @@
 #Placeholder for a model
+class Post < ActiveRecord::Base
+  # attr_accessor :name, :content
+
+  # POSTS = []
+
+  # def initialize(params)
+  #   @name = params[:name]
+  #   @content = params[:content]
+  #   POSTS << self
+  # end
+
+  # def self.all
+  #   POSTS
+  # end
+
+end
