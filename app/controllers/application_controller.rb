09313b624c1ba4e34a40bce27a35d269d0d29a49
Brian Holland <brian.holland@proatria.com>
2016-03-10 17:07:05 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,45 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/?' do
+    erb :posts
   end
+
+  get '/posts/new/?' do
+    erb :new
+  end
+
+  post '/posts' do
+    @post = Post.create(params)
+    erb :posts
+  end
+
+  get '/posts/?' do
+    erb :posts
+  end
+
+  get '/posts/:id/?' do
+    @post = Post.find(params[:id])
+    erb :post
+  end
+
+  get '/posts/:id/edit/?' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.update({:name => params[:name], :content => params[:content]})
+    erb :post
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    deleted_name = @post.name
+    @post.delete
+    "Post #{deleted_name} was deleted. <a href='/'>BLOGOSPHERE</a> "
+  end
+
+
 end
