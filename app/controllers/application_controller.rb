a79e4cd6747c762c956704d11063265f467eeefa
glee38 <glee3388@gmail.com>
2016-04-01 17:58:07 -0400

    add comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,43 +7,39 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-  
-  end
-
-  get '/posts/new' do
+  get '/posts/new' do # loads new form
     erb :new  
   end
 
-  post '/posts' do
+  post '/posts' do # creates a post
     #raise params.inspect
     Post.create(params)
     redirect '/posts'
   end
 
-  get '/posts' do
+  get '/posts' do # loads index page
     @posts = Post.all
     erb :index
   end
 
-  get '/posts/:id' do
+  get '/posts/:id' do # loads show page
     @post = Post.find(params[:id])
     erb :show
   end
 
-  get '/posts/:id/edit' do
+  get '/posts/:id/edit' do # loads edit form
     @post = Post.find(params[:id])
     erb :edit
   end
 
-  patch '/posts/:id' do
+  patch '/posts/:id' do # updates a post
     @post = Post.find(params[:id])
     @post.update(name: params[:name], content: params[:content])
     @post.save
     redirect 'posts/' + @post.id.to_s
   end
 
-  delete '/posts/:id/delete' do
+  delete '/posts/:id/delete' do # delete action
     @post = Post.find(params[:id]).destroy
     erb :deleted
   end
