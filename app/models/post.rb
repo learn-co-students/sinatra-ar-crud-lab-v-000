f7ccf7a12dbc84db1cb338de43b9f744183007e2
Enoch Griffith <enoch2k2@hotmail.com>
2016-04-04 03:24:37 +0000

    Done.

---
+++ app/models/post.rb
@@ -0,0 +1,16 @@
+class Post < ActiveRecord::Base
+#   attr_accessor :name, :content
+
+#   @@all = []
+
+#   def self.all
+#     @@all
+#   end
+
+#   def initialize(params)
+#     self.name = params[:name]
+#     self.content = params[:content]
+#     @@all << self
+#   end
+
+end
