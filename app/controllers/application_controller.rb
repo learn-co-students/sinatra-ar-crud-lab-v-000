e84d078b72070af7a79259e2ce1d71df721fc9a5
mirandalily <mirandaraymond@me.com>
2016-04-18 20:16:06 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,46 +7,41 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-   erb :index
- end
+  get '/posts/new' do
+      erb :new
+    end
 
- get '/posts/new' do
-   erb :new
- end
+    post '/posts' do
+      @post = Post.create(params)
+      redirect to '/posts'
+    end
 
- post '/posts' do
-   Post.create(name: params[:name], content: params[:content])
-   @posts = Post.all
-   erb :index
- end
+    get '/posts' do
+      @posts = Post.all
+      erb :index
+    end
 
-  get '/posts' do
-   @posts = Post.all
-   erb :index
- end
+    get '/posts/:id' do
+      @post = Post.find(params[:id])
+      erb :show
+    end
 
- get '/posts/:id' do
-   @post = Post.find(params[:id])
-   erb :show
- end
+    get '/posts/:id/edit' do
+      @post = Post.find(params[:id])
+      erb :edit
+    end
 
- patch '/posts/:id/edit' do
-   @post = Post.find(params[:id])
-   @post.update(name: params[:name], content: params[:content])
-   redirect '/posts/' +  @post.id
- end
+    patch '/posts/:id' do
+      @post = Post.find(params[:id])
+      @post.update(name: params[:name], content: params[:content])
+      redirect to ("/posts/#{@post.id}")
+    end
 
- get '/posts/:id/edit' do
-   @post = Post.find(params[:id])
-
-   erb :edit
- end
-
- delete '/posts/:id/delete' do
-   @post = Post.find(params[:id])
-   @post.destroy
-   erb :delete
- end
+    delete '/posts/:id/delete' do
+      @post = Post.find(params[:id])
+      @name = @post.name
+      @post.destroy
+      erb :deleted
+    end
 
 end
