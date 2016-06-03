b22f87d6f90b5b822df55f0c085fbfd4f1e43738
Yumei Leventhal <yleventhal@ymail.com>
2016-06-03 12:45:46 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,12 +12,15 @@ class ApplicationController < Sinatra::Base
   # end
 
   get '/posts/new' do
-    erb :view
+    erb :new
   end
 
   post '/posts' do
     @post = Post.create(params)
+    # @name = params[name]
+    # @content = params[content]
     redirect to '/posts'
+    # erb :index
   end
 
   get '/posts' do
@@ -25,12 +28,36 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+
   get '/posts/:id' do
     @post = Post.find(params[:id])
     erb :show
   end
 
-  get 'posts/:id/edit' do
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
 
-  end  
+  patch '/posts/:id' do
+    # @post_edit = Post.find(params[:id])
+    # @post_edit.name = params[:name]
+    # @post_edit.content = params[:content]
+    # @post_edit.save
+    # erb :show
+    @post = Post.find(params[:id])
+    @post.update(name: params[:name], content: params[:content])
+    # redirect to ("/posts/#{@post.id}")
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @name = @post.name
+    @post.destroy
+    erb :deleted
+    # @post_delete = Post.find(params[:id])
+    # @post_delete.destroy
+    # erb :deleted
+  end
 end
