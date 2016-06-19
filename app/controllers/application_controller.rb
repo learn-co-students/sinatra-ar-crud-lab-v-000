d6d9ed91ec51d7cebad9e3ce0feae699a45c1097
Motti Mordechay Gottlieb <mottigottlieb@gmail.com>
2016-06-19 17:32:13 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,22 +10,15 @@ class ApplicationController < Sinatra::Base
   #   @post1 = Post.create(:name => post_name, :content => post_content)
   #   @post2 = Post.create(:name => "second post", :content => "i'm a really good blogger")
   # end
-  get '/' do
-
-  end
-
   get '/posts/new' do
    #RENDER NEW.EB
     erb :new
   end
 
-  post '/posts' do
-    @name = params[:name]
-    @content = params[:content]
-    Post.create(name: @name, content: @content)
-     erb :posts
-  end
-
+  post '/posts' do  #creates a post
+   @post = Post.create(params)
+   redirect to '/posts'
+ end
   get '/posts' do
     @posts = Post.all
     erb :index
@@ -34,6 +27,34 @@ class ApplicationController < Sinatra::Base
   get '/posts/:id' do
     @post = Post.find_by_id(params[:id])
     erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+
+  get '/posts/:id/edit' do #loads edit form
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+
+    patch '/posts/:id' do  #updates a post
+      @post = Post.find_by_id(params[:id])
+      @post.name = params[:name]
+      @post.content = params[:content]
+      @post.save
+      erb :show
+    end
+
+
+  delete '/posts/:id/delete' do #delete action
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :deleted
+
   end
 
 end
