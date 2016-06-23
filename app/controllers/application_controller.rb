50c7301a4933166168067d88c465b184e2452410
Christian Whitesides <whitesidescr@gmail.com>
2016-06-23 16:57:29 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,43 +7,46 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/posts/new' do 
+  get '/posts/new' do # loads new form
     erb :new
   end
 
-  post '/posts' do
+  post '/posts' do # creates a post
     attributes = {
       name: params[:name],
       content: params[:content]
     }
     Post.create(attributes)
 
-    erb :index
+    redirect to '/posts'
   end
 
-  get '/posts' do
+  get '/posts' do # loads index page
      @posts = Post.all
      erb :index
   end
 
-  get '/posts/:id' do
-    @post = Post.find(params[:id])
+  get '/posts/:id' do # loads show page
+    @post = Post.find_by_id(params[:id]) # find_by_id is a dynamic finder (see 15 of http://guides.rubyonrails.org/active_record_querying.html)
     erb :show
   end
 
-  get '/posts/:id/edit' do
-    @post = Post.find(params[:id])
+  get '/posts/:id/edit' do # loads edit form
+    @post = Post.find_by_id(params[:id])
     erb :edit
   end
 
-  patch '/posts/:id' do
-    Post.update(params[:id], name: params[:name], content: params[:content])
+  patch '/posts/:id' do # updates a post
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
     erb :show
   end
 
-  delete '/posts/:id/delete' do
-    @post = Post.find(params[:id])
+  delete '/posts/:id/delete' do # deletes a post
+    @post = Post.find_by_id(params[:id])
     @post.destroy
-    erb :show
+    erb :deleted
   end
 end
