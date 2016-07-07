984727b4779dc749624218852c4dd081f1ea44b7
andrewstone8 <andrewstone8@gmail.com>
2016-07-07 22:39:50 +0900

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -8,4 +8,8 @@ ActiveRecord::Base.establish_connection(
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
+configure :development do
+  set :database, "sqlite3:db/migrate/create_posts.db"
+end
+
 require_all 'app'
