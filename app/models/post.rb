c423efb85314d5f338a68a120bce0cfe1130f464
Yechiel Kalmenson <chilikkal@gmail.com>
2016-07-20 02:39:55 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,10 @@ class Post < ActiveRecord::Base
   attr_accessor :name, :content
   @@all = []
 
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
-    @@all << self
-  end
+  # def initialize(attributes)
+  #   attributes.each {|key, value| self.send(("#{key}="), value)}
+  #   @@all << self
+  # end
 
   def self.all
     @@all
