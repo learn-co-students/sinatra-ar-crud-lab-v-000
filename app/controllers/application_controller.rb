da8249fb5cc4ed4780348d2bb79da1968aef968d
Yechiel Kalmenson <chilikkal@gmail.com>
2016-07-20 02:29:00 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,15 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+    "Hello World"
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    erb :posts
   end
 end
---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ActiveRecord::Base
+  attr_accessor :name, :content
+  @@all = []
+
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
