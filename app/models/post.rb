f0a95b2c6ad282a10d61a3801407ff2c0266e879
Caleb Faught <faughtcj@gmail.com>
2016-08-03 17:01:26 +0000

    Done.

---
+++ app/models/post.rb
@@ -0,0 +1,12 @@
+class Post < ActiveRecord::Base
+  # attr_accessor :name, :content
+  #
+  # @@all = []
+  #
+  # def initialize(name, content)
+  #   @name = name
+  #   @content = content
+  #
+  #   @@all << self
+  # end
+end
