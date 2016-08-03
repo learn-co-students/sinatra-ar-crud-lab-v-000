f7945b8293a159d08c40310c01f425b8fc2e41e9
peterberwind <peterberwind@gmail.com>
2016-08-03 14:08:59 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,25 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
   end
+
+  # New
+  get '/posts/new' do
+    erb :new
+  end
+
+  # Index
+  post '/posts' do
+    Post.new(name: params[:name], content: params[:content])
+    erb :index
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,1 +1,19 @@
 #Placeholder for a model
+
+class Post < ActiveRecord::Base
+  # attr_accessor :name, :content
+
+  # @@posts = []
+  #
+  # def initialize(args)
+  #   # @name = args[:name]
+  #   # @content = args[:content]
+  #   args.each {|key, value| self.send(("#{key}="), value)}
+  #   @@posts << self
+  # end
+
+  # def self.all
+  #   @@posts
+  # end
+
+end
