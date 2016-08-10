ac3dccf69c5a4a5e7aff51c0b519413ca8b21dde
DownsideupmediaUSA <brandondecarlo@live.com>
2016-08-10 10:39:58 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,47 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/posts/new' do
+    erb :new
   end
+
+  post '/posts' do #(creates)
+    p params
+    p params[:post]
+    post = Post.create(params[:post])
+    redirect("/posts/#{post.id}")
+  end
+
+
+  get '/posts' do #(index page)
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  #-------- update------->
+ 
+ get '/posts/:id/edit' do
+  @post = Post.find(params[:id])
+  erb :edit
+end
+
+ post '/posts/:id' do
+   @post = Post.find(params[:id]) 
+   @post.name = params[:name]
+   @post.content = params[:content]
+   @post.save
+   erb :show
+ end
+
+
+
+
+
+
+
 end
---
+++ db/migrate/20160809163945_posts.rb
@@ -0,0 +1,8 @@
+class Posts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :content
+    end
+  end
+end
