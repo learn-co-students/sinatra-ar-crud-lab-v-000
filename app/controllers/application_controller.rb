05de07e5825fb062ef5c44c9a6fab3ff0fdcc759
Shmuwol <shmullywolfson@gmail.com>
2016-08-10 11:45:50 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,4 +43,10 @@ class ApplicationController < Sinatra::Base
 
     redirect "/posts/#{@post.id}"
   end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :deleted
+  end
 end
