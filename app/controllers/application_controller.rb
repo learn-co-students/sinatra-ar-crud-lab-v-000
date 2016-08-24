b3beb07dbcccfc5c31dbe58337727156a67649bd
jbok4 <jbok4@aol.com>
2016-08-24 16:38:05 -0400

    reviewed, cleaned up views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,17 +7,13 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-    get '/' do
-
-  end
-
   get '/posts/new' do 
     erb :new
   end
 
   post '/posts' do  #creates a post
-      @post = Post.create(params)
-      redirect to '/posts'
+    @post = Post.create(params)
+    redirect to '/posts'
   end
 
   get '/posts' do
