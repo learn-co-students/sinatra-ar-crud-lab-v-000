6030b215073478be04ea592b1a3de37596e76ee8
bmolina-nyc <bruce.d.molina@gmail.com>
2016-08-27 20:09:19 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,38 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/posts/new' do 
+    erb :new
   end
+
+  post '/posts' do 
+    new_post = Post.create(params)
+    erb :index
+  end
+
+  get '/posts' do 
+    @posts = Post.all 
+    erb :index
+  end
+
+
+# Create the get '/posts/:id' controller action. This action should use Active Record to grab the 
+# post with the id that is in the params and set it equal to @post. Then, it should render the 
+# show.erb view page. That view should use erb to render the @post's title and content.
+
+  get '/posts/:id' do 
+    id = params[:id].to_i
+    @post = Post.find_by id: id
+    erb :show 
+  end
+
+  get '/posts/:id/edit' do 
+
+
+    erb :edit 
+  end
+
+
+
 end
+
