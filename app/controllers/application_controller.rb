74894789616c5761f3d954f34a68ce0430798e98
Lani1321 <lani.berry13@gmail.com>
2016-09-27 15:36:42 -0400

    Implement Create, Read, and Delete actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,51 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  # Create
+  get '/posts/new' do 
+    erb :new
   end
+ 
+  post '/posts' do 
+    @post = Post.create(params)
+    erb :index
+  end
+
+  # Read
+  # Instance variable will access all the posts in view
+  get '/posts' do 
+    @posts = Post.all
+    erb :index
+  end
+
+  # Read
+  get '/posts/:id' do 
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  # Update
+  get '/posts/:id/edit' do 
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  # When I use patch, it doesn't work
+  patch '/posts/:id' do 
+    @post = Post.find(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    redirect "/posts/#{@post.id}"
+    # erb :show
+  end
+
+  post '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+
+    @post.destroy
+
+    erb :deleted
+  end
+
 end
