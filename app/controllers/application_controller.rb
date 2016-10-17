f7c09cd3b2747beec64cba395aff96dbb45b1214
Perla Yates <pianistps@yahoo.com>
2016-10-17 23:22:14 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,31 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
+  end
+
+  get '/posts/new' do #loads new form
+    erb :new
+  end
+
+  post '/posts' do #creates post
+    binding.pry
+    @post = Post.create(name: params[:name], content: params[:content])
+    redirect to '/posts'
+  end
+
+  get '/posts' do #loads index page
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id/edit' do
+    erb :edit
+  end
+
+  delete '/posts/:id/delete' do
+
+
   end
 end
---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+#Placeholder for a model
+
+class Post < ActiveRecord::Base
+  attr_accessor :name, :content
+end
