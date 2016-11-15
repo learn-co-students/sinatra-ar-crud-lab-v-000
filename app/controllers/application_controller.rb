d7ddb201ab9f25b5e76ef4db6959909014124d3e
Elyse Klova <elyse.klova@gmail.com>
2016-11-15 03:17:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,26 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    @posts = []
+    post = Post.create(name: params[:name], content: params[:content])
+    @posts << post
+    erb :index
+  end
+
+  get '/posts' do
+    @posts = []
+    Post.find_each do |post|
+      @posts << post
+    end
+    erb :index
   end
 end
