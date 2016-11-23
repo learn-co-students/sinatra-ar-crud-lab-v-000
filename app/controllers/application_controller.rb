2ed10173bb50a4ba801deda30dec9e402476df9e
eleanorgraham <eleanorcjkgraham@gmail.com>
2016-11-23 11:17:06 -0500

    deleting :x: - all tests passing, although they don't exactly match the directions...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,22 +18,30 @@ class ApplicationController < Sinatra::Base
 
   get '/posts' do
     @posts = Post.all
+    @deleted = Post.deleted
     erb :index
   end
 
   get '/posts/:id' do
-    @post = Post.all.select {|post| post.id == params[:id].to_i}.first
+    @post = Post.all.find_by_id(params[:id])
     erb :show
   end
 
   get '/posts/:id/edit' do
-    @post = Post.all.select {|post| post.id == params[:id].to_i}.first
+    @post = Post.all.find_by_id(params[:id])
     erb :edit
   end
 
   patch '/posts/:id' do
-    @post = Post.all.select {|post| post.id == params[:id].to_i}.first
+    @post = Post.all.find_by_id(params[:id])
     @post.update(name: params[:name], content: params[:content])
     erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.all.find_by_id(params[:id])
+    @post.add_to_deleted
+    @post.delete
+    redirect '/posts'
   end
 end
