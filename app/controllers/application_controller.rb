b426a6ccd414c2c4e0222480f6aa9e2a53c99453
Senad Hadrovic <github email address>
2017-01-09 09:09:53 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,46 @@ class ApplicationController < Sinatra::Base
   get '/' do 
     
   end
+
+  get '/posts/new' do 
+  	erb :new
+  end 
+
+  post '/posts' do 
+  	Post.create(name: params["name"], content: params["content"])
+  	@posts = Post.all
+  	# binding. pry
+  	erb :index
+  end 
+
+  get '/posts' do 
+  	@posts = Post.all
+  	erb :index
+  end 
+
+  get '/posts/:id' do 
+  	@post = Post.find(params[:id])
+  	erb :show
+  end 
+
+  get '/posts/:id/edit' do 
+  	@post = Post.find(params[:id])
+  	erb :edit 
+  end 
+
+  patch '/posts/:id' do 
+  	@post = Post.find(params[:id])
+  	@post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end 
+
+  delete '/posts/:id/delete' do 
+  	@post = Post.find(params[:id])
+  	@post.delete
+  	erb :deleted
+  end 
+
+
 end
