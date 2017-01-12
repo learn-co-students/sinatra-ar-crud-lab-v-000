6fa1ad1b573037879bf6b6fd6ab361cffb3f3f2c
mjdele <mike.delehant@gmail.com>
2017-01-12 01:17:58 -0500

    Done.

---
+++ app/models/post.rb
@@ -0,0 +1,16 @@
+class Post < ActiveRecord::Base
+#  attr_accessor :name, :content
+
+ # POSTS = []
+
+#  def initialize(name, content)
+#    @name = name
+#    @content = content
+#    POSTS << self
+#  end
+
+ # def self.all
+ #   POSTS
+ # end
+
+end
