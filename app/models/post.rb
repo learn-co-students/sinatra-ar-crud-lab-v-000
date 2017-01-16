bec780ef7afd6683c321b11a373e98cf0bab19ff
Mary Umoh <umohm12@gmail.com>
2017-01-16 23:23:55 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,3 @@
 class Post < ActiveRecord::Base
-  #validates :name, :content, presence: true
-  #attr_accessor :name, :content
-
-  #POSTS = []
-
-  #def initialize(name, content)
-  #  @name = name
-  #  @content = content
-  #  POSTS << self
-#  end
-
-#  def self.all
-#    POSTS
-#  end
+  
 end
