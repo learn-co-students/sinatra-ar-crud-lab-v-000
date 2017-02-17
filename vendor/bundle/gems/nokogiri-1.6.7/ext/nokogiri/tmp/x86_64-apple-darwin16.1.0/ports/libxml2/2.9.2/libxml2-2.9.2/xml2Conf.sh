c40dd7f8e0547aecca7348463d075d5a6e27a286
M. Simon Borg <msimonborg@gmail.com>
2017-02-17 00:43:50 -0500

    Done.

---
+++ vendor/bundle/gems/nokogiri-1.6.7/ext/nokogiri/tmp/x86_64-apple-darwin16.1.0/ports/libxml2/2.9.2/libxml2-2.9.2/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/matthewborg/developing/learn.co/sinatra/sinatra-ar-crud-lab-v-000/vendor/bundle/gems/nokogiri-1.6.7/ports/x86_64-apple-darwin16.1.0/libxml2/2.9.2/lib"
+XML2_LIBS="-lxml2 -lz -lpthread  -liconv -lm "
+XML2_INCLUDEDIR="-I/Users/matthewborg/developing/learn.co/sinatra/sinatra-ar-crud-lab-v-000/vendor/bundle/gems/nokogiri-1.6.7/ports/x86_64-apple-darwin16.1.0/libxml2/2.9.2/include/libxml2"
+MODULE_VERSION="xml2-2.9.2"
+
