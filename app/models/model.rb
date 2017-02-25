e9da4b6c597cf8afcead74d4c01e31d1015ee04d
Kevin Adkins <kevinladkins@gmail.com>
2017-02-25 20:26:16 +0000

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,21 @@
 #Placeholder for a model
-class Post < ActiveRecord::Base
+class Model < ActiveRecord::Base
+
+  attr_accessor :name, :content
+  @@all = []
+
+  def initialize(attributes)
+    attributes.each do |k, v|
+      self.send(("#{k}="), v)
+    end
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
 end
