59df505773a7f0c58efe7b7c5039c2dee69fbe48
vicision <duncan.j.mclachlan@gmail.com>
2017-03-06 17:49:47 -0500

    Done: all tests passing

---
+++ app/models/post.rb
@@ -0,0 +1,25 @@
+class Post < ActiveRecord::Base
+  # attr_reader :name, :content
+  #
+  # # POSTS = []
+  #
+  # def initialize(params)
+  #   @name = params[:name]
+  #   @content = params[:content]
+  # end
+
+  # def self.create(params)
+  #   self.new(params)
+  #   POSTS << self
+  # end
+
+  # def self.all
+  #   POSTS
+  # end
+  #
+  # def self.last
+  #   self.all[-1]
+  # end
+  #
+
+end
