21c513a1dd8c9d890588ecd3e461da8e4f6b6234
Bryan Hou <bryan.hou94@gmail.com>
2017-04-23 21:48:25 +0000

    in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,38 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    attributes = {name: params[:post][:name], content: params[:post][:content]}
+    Post.create(attributes)
+    erb :index
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @id = params[:id]
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @id = params[:id]
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @id = params[:id]
+    Post.find_by(id: @id).update(name: params[:post][:name], content: params[:post][:content])
+    erb :show
   end
 end
