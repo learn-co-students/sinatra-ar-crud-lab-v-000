b0d9e7f6d4572830719a3007cd170fd760825c4e
Joseph Hyatt <jhyatt1017@gmail.com>
2017-06-09 03:10:54 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
+#####Create Action
+#creates a new blog post
+#redirects to '/posts'
+#####Read Action
+#####index action
+#responds with a 200 status code
+#displays all the blog posts
+#show action
+#show page responds with a 200 status code
+#show page displays the post's name
+#show page displays the post's content
+#####update action
+#responds with a 200 status code
+#displays the existing object in the edit form
+#saves edits to a blog post
+#redirects to '/posts/:id'
+#submits the form via a patch request
+#####delete action
+#responds with a 200 status code
+#deletes a blog post from the database
+#displays a view telling us which post was deleted
+#submits the form via a delete request
+
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -7,7 +30,44 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    @post = Post.create(params)
+    erb :show
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    Post.update(@post.id, name: params[:name], content: params[:content])
+    redirect to("/posts/#{@post.id}")
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    Post.delete(params[:id])
+    erb :delete
+  end
+
 end
