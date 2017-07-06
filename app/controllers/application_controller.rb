ef75c5192e03e5314d83123d1b9013fd6bbfdb6d
Aimee Carney <acarney29@gmail.com>
2017-07-06 04:13:12 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,17 +26,17 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/:id' do
-    @post = Post.find_by(params[:id])
+    @post = Post.find_by_id(params[:id])
     erb :show
   end
 
   get '/posts/:id/edit' do
-    @post = Post.find_by(params[:id])
+    @post = Post.find_by_id(params[:id])
     erb :edit
   end
 
   patch '/posts/:id' do
-    @post = Post.find_by(params[:id])
+    @post = Post.find_by_id(params[:id])
     @post.name = params[:name]
     @post.content = params[:content]
     @post.save
@@ -44,7 +44,7 @@ class ApplicationController < Sinatra::Base
   end
 
   delete '/posts/:id/delete' do
-    @post = Post.find_by(params[:id])
+    @post = Post.find_by_id(params[:id])
     @post.delete
     erb :delete
   end
