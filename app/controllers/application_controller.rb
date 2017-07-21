dae419bda78644f4a15591322635ce4398e10cc7
charlie lee <ctlee9096@gmail.com>
2017-07-21 03:55:55 +0000

    testing commit to check solution

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -7,7 +8,20 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+
+
+  get '/posts/new' do
+    binding.pry
+    #@post2 = Post.new(params)
+    #post1 = post1
+    #@post2 = post2
+    #post1.save
+    #post2.save
+  erb :new
+  end
+
+  post '/posts' do
+    @post = Post.create(params)#how to Create in AR
+    erb :index
   end
 end
