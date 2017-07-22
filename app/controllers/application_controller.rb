dff6fe949889cdfe14afb0a9741ae175cd819031
charlie lee <ctlee9096@gmail.com>
2017-07-22 19:44:06 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 require_relative '../../config/environment'
-
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -7,7 +7,48 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do #homepage
+    erb :index
   end
+
+  get '/posts/new' do #loads new form
+    erb :new
+  end
+
+  post '/posts' do #creates a post
+    @post = Post.create(params)
+    redirects to '/posts'
+  end
+
+  get '/posts' do #loads index page
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do#show action
+  @post = Post.find(params[:id])
+  #binding.pry
+    erb :show
+  end
+
+  get '/posts/:id/edit' do#Update: Loads form for edit
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+  post '/posts/:id' do  #updates a post
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+
+    erb :show
+  end
+  # 
+  # post '/posts/:id/delete' do #delete action
+  # @post = Post.find_by_id(params[:id])
+  # @post.delete
+  # erb :deleted
+end
+
 end
---
+++ to_do.md
@@ -0,0 +1,7 @@
+list
+  - has many list items
+   -a lot of list items
+list_item
+  -belong to list
+  -name
+  -list_id = foreign key
