91dd5ea5e59e8456ba1b0c1e12443056aed1a02c
charlie lee <ctlee9096@gmail.com>
2017-07-22 04:28:27 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 require_relative '../../config/environment'
-require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -8,20 +7,41 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-
-
   get '/posts/new' do
-    binding.pry
-    #@post2 = Post.new(params)
-    #post1 = post1
-    #@post2 = post2
-    #post1.save
-    #post2.save
-  erb :new
+    erb :new
   end
 
   post '/posts' do
-    @post = Post.create(params)#how to Create in AR
+    @post = Post.create(params)
     erb :index
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post_edit = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post_edit = Post.find(params[:id])
+    @post_edit.name = params[:name]
+    @post_edit.content = params[:content]
+    @post_edit.save
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    @post_delete = Post.find(params[:id])
+    @post_delete.destroy
+    erb :deleted
   end
 end
