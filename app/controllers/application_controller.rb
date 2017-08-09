3d23058c7563500aea0f0959a6747263c49da3f2
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-08-09 20:46:25 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,22 +11,26 @@ class ApplicationController < Sinatra::Base
   get '/' do
 
   end
-
+#This is the C -(create) in the CRUD
+#This is where the view that provides the form for the user's input
   get '/posts/new' do
+# get '/' do *** Show something ***
     erb :new
   end
-
+#This is where the user's input is used to instantiate a new object
   post '/posts' do
+# post '/' do *** Create something ***
     @post = Post.create(name: params[:name], content: params[:content])
     redirect to '/posts'
   end
 
+#This begins the R -(read) in the CRUD
   get '/posts' do
     @posts = Post.all
     erb :index
   end
 
-  
+
 
   #get 'posts/:id' do # This will load the show page
   #  @post = Post.find_by_id(params[:id])
@@ -37,3 +41,8 @@ class ApplicationController < Sinatra::Base
 
 
 end
+
+# put '/' do *** Replace something ***
+# patch '/' do *** Modify something ***
+# delete '/' do *** Annihilate something ***
+# link: http://www.sinatrarb.com/intro.html
