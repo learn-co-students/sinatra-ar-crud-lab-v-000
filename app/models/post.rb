c4ae6575aaf927db497993fc97f34d4e1abb45fc
psnider30 <pbsnider@gmail.com>
2017-08-19 14:00:11 -0700

    Done.

---
+++ app/models/post.rb
@@ -0,0 +1,20 @@
+class Post < ActiveRecord::Base
+
+  # @@posts = []
+  #
+  # def initialize(blog_post)
+  #   @name = blog_post[:name]
+  #   @content = blog_post[:content]
+  #
+  #   @@posts << self
+  # end
+  #
+  # def self.all
+  #   @@posts
+  # end
+  #
+  # def self.clear
+  #   @@posts.clear
+  # end
+
+end
