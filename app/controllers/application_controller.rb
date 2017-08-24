3d631a537454ff34a75b213457cb50bacd56cc40
Nicholas D'Amico <nickalan82@icloud.com>
2017-08-24 00:54:10 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,56 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  configure do
+    enable :session
+    set :session_secret, "secret"
   end
+
+  get '/' do
+
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  get '/posts/:id' do
+      @post = Post.find(params[:id])
+
+      erb :show
+  end
+
+  post '/posts' do
+    Post.create(params)
+    @posts = Post.all
+
+    erb :index
+  end
+
+  get '/posts' do
+    @posts = Post.all
+
+    erb :index
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.update(name: params[:name])
+    @post.update(content: params[:content])
+    redirect to('/posts/' + @post.id)
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.delete
+
+    erb :delete
+  end
+
 end
