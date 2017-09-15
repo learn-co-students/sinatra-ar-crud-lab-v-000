ca7c82a6ba9c30109b99039a69bf5e1f7483ac26
Erica Plofsky <ericaplofsky@gmail.com>
2017-09-15 22:30:34 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,53 @@ require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+    configure do
+      set :public_folder, 'public'
+      set :views, 'app/views'
+    end
 
-  get '/' do 
-    
+    get '/' do
+      @posts = Post.all
+      erb :index
+    end
+
+    get "/posts" do
+      @posts = Post.all
+      erb :index
+    end
+
+    get "/posts/new" do
+      erb :new
+    end
+
+    get "/posts/:id" do
+      @post = Post.find(params[:id])
+      @show_edit_button = true
+      erb :show
+    end
+
+    get "/posts/:id/edit" do
+      @post = Post.find(params[:id])
+      erb :edit
+    end
+
+    post "/posts" do
+      Post.create(params)
+      @posts = Post.all
+      erb :index
+    end
+
+    patch '/posts/:id' do
+      @post = Post.find_by_id(params[:id])
+      @post.name = params[:name]
+      @post.content = params[:content]
+      @post.save
+      erb :show
+    end
+
+    delete '/posts/:id/delete' do
+      @post_delete = Post.find(params[:id])
+      @post_delete.destroy
+      erb :delete
+    end
   end
-end
