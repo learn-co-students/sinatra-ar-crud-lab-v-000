238ff521bf08118123918e5e3497f38bf1cbfcc4
Z. Drake <zeedrake3000@gmail.com>
2017-09-21 00:36:12 -0500

    App ahs several new routes the new which gives the user a form to add a new post, upon submission the app takes the uses to the index page, which contains all the posts in the Posts table. The user can also view just one post, by going to the /posts/:id page id is replaced with a number and the show page presents the user with that specific post.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,45 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
   end
+
+  # create
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    # binding.pry
+    # manual way
+    # @post = Post.new(params)
+    # params => {"name"=>"New Post", "content"=>"Here is the content"}
+    # @post = => #<Post:0x007fbb0cdc29f8 id: nil, name: "New Post",
+      # content: "Here is the content">
+
+    # AFTER SAVE: @post = => #<Post:0x007fbb0cdc29f8 id: 1, name: "New Post",
+      # content: "Here is the content">
+    # @post.save
+
+    @post = Post.create(params)
+
+    # send user to main posts page that contains all the posts
+    redirect to '/posts'
+  end
+
+  # read
+  get '/posts' do
+    @posts = Post.all
+    # binding.pry
+    erb :index
+  end
+
+  #  grab the post with the id that is in the params and set it equal to @post
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+
+    erb :show
+  end
+
 end
