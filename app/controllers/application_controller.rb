ae3eff54101629222808eddbdb7369623a855aff
Elvin <Dimix181@Gmail.com>
2017-10-18 14:48:16 -0400

    all test are passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/posts' do
+    erb :'posts/index'
   end
+
+  get '/posts/new' do
+    erb :'posts/new'
+  end
+
+  post '/posts/new' do
+  @post = Post.create(name:params[:name],content:params[:content])
+  @post.save
+      redirect to '/posts'
+  end
+
+  get '/posts/:id' do
+      @post = Post.all.find(params[:id])
+      erb :'posts/show'
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.all.find(params[:id])
+    erb :'posts/edit'
+  end
+
+  patch '/posts/:id' do
+      post = Post.find(params[:id])
+      post.update({name:params[:name], content:params[:content] })
+      redirect to "/posts/#{params[:id]}"
+  end
+
+  delete '/posts/:id/delete' do
+    post = Post.find(params[:id])
+    post.destroy
+    "#{post.name} was deleted"
+  end
+
+
+
 end
