6c612ca33e4a7883ffa1e83ab3c4231911aa4e34
John Tuttle <thejwt@gmail.com>
2017-11-03 17:17:50 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -7,7 +8,49 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
   end
+
+  post '/posts' do
+    post = Post.new(params)
+    post.save
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find_by(id: params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find_by(id: params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    post = Post.find_by(id: params[:id])
+    post.update(name: params[:name], content: params[:content])
+    #binding.pry
+    @post = Post.find_by(id: params[:id])
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    post = Post.find_by(id: params[:id])
+    @post_name = post.name
+    post.destroy
+    "#{@post_name} was deleted"
+  end
+
 end
