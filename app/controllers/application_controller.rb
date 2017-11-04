9ddf51c6f5a6494a8952b1ce5f1264aca96fce41
Yvonne Dreptate <YvonneDreptate@gmail.com>
2017-11-04 12:18:15 -0400

    partial passed lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,36 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+
+
+  get '/posts/new' do #new form
+    erb :new
   end
+
+  post '/posts' do #create a post
+    @post = Post.create(params)
+    redirect to '/posts'
+  end
+
+  get '/posts' do #load index page
+    @posts= Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do  #loads show page
+     @post = Post.find_by_id(params[:id])
+     erb :show
+   end
+
+   get '/posts/:id/edit' do
+
+     erb :edit
+   end
+
+   patch 'posts/:id' do
+     
+   end
+
+
+
 end
