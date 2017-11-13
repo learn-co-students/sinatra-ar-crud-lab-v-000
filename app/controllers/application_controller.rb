c8f976ae02f8fd4c0f21b739d19c224f3f223fc7
alpbourne <alpbourne@gmail.com>
2017-11-13 18:29:28 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,14 @@ class ApplicationController < Sinatra::Base
   get '/posts/new' do
     erb :new
   end
-  
+
+  post '/posts' do
+    @post = Post.create(params)
+    redirect to '/posts'
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
 end
