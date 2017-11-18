4a60b76ccb182294d5c047c68e16076c54ce1117
Vicky Lau <vlau.nyc@gmail.com>
2017-11-18 02:35:20 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,46 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+     redirect to '/posts' #index page with all blog posts
   end
+
+  get '/posts/new' do #new form for blog post
+    erb :new
+  end
+
+  post '/posts' do #Create Action: creates a new blog post
+    @post = Post.create(params)
+    redirect to '/posts'
+  end
+
+  get '/posts' do #Read Action: Index action: displays all the blog posts
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do #Read Action: show action: displays post by .id
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do #Update Action: displays the existing object in the edit form
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+  post '/posts/:id' do  #Update Action: saves edits to a blog post
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+  post '/posts/:id/delete' do #Delete Action: deletes a blog post from the database
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :deleted
+  end
+
 end
