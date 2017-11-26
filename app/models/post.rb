8a45b38c12ab3caa52928c2d185035368e5474b3
Jessica Bensch <jessica.bensch11@gmail.com>
2017-11-26 12:42:47 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,10 @@
 class Post < ActiveRecord::Base
 
-  attr_accessor :name, :content
 
-  @@posts = []
 
-  def initialize(params)
-    @id = params[:id]
-    @name = params[:name]
-    @content = params[:content]
-    @@posts << self
-  end
 
-  def self.all
-    @@posts
-  end
+
+
+
 
 end
