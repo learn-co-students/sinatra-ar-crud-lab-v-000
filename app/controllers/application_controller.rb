266f5be8505072b0f2ebb93c90032aeaecbc5fa8
Jacilyn Hayden <jacihayden@gmail.com>
2017-11-28 04:37:29 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,8 +45,9 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  post '/posts/:id/delete' do
+  delete '/posts/:id/delete' do
     @post = Post.find(params[:id])
+    @post.delete
     erb :delete
   end
 end
