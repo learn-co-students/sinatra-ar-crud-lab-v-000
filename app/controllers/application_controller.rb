c65df1a5278ef9999444fb9c3ece6b5f47052945
Emma Feng <ff18qd@gmail.com>
2017-11-28 03:55:08 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -12,7 +13,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/new' do
-
     erb :new
   end
 
@@ -21,19 +21,32 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/post/:id' do
-    @post = Post.find(params[:id])
+  post '/posts' do
+    @post = Post.create(params)
+    @posts = Post.all
+    # binding.pry
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
     erb :show
   end
 
   get '/posts/:id/edit' do
-    @post = Post.find(params[:id])
-
+    @post = Post.find_by_id(params[:id])
     erb :edit
   end
 
+  patch '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    @post.update(name: params[:name], content: params[:content])
+    erb :show
+  end
+
   delete '/posts/:id/delete' do
-    @post = Post.find(params[:id])
+    @post = Post.find_by_id(params[:id])
+    @post.delete
     erb :delete
   end
 
