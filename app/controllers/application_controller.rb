32cdda1cd81c8a8791943a750734f7303276a3d1
Mel Rudick <melissarudick@MacBook-Air.local>
2017-12-10 15:25:30 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,40 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get "/posts/new" do # makes new form
+    erb :new
+  end
+
+  post "/posts" do # creates a post
+    @post = Post.create(name: params[:name], content: params[:content])
+    redirect "/posts"
+  end
+
+  get "/posts" do # loads index page
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do # shows all posts
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
+
+  post '/posts/:id/edit' do # shows edit form
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+  post '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    @post.name(params[:name])
+    @post.content(params[:content])
+    @post.save
+  end
+
+  post '/posts/:id/delete' do
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :delete
   end
 end
