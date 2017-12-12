47f498a60aaecf9ca8f813d24bd3f049d19ce3a8
EC2 Default User <ec2-user@ip-172-31-32-173.us-east-2.compute.internal>
2017-12-12 18:16:35 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,52 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do 
-    
   end
+  
+    
+  get '/posts' do
+    @posts = Post.all
+    
+    erb :index
+  end
+  
+  post '/posts' do
+    @posts = Post.all
+    Post.create(name: params[:name], content: params[:content])
+    
+    erb :index
+  end
+  
+  get '/posts/new' do
+    erb :new
+  end
+  
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    
+    erb :show
+  end
+  
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+
+    erb :edit
+  end
+  
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    
+    @post.update(name: params[:name], content: params[:content])
+    
+    erb :show
+  end
+  
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.destroy
+    
+    erb :delete
+  end
+  
+  
 end
