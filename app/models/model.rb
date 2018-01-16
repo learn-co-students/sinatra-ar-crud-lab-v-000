43a41dc94e4f1ee8b1dadcd0f52daa333b56fbec
Caroline Shields <caroline.shields@amkor.com>
2018-01-16 00:10:35 +0000

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,19 +1,3 @@
 #Placeholder for a model
 class Post < ActiveRecord::Base
-
-  attr_accessor :name, :content
-
-  Posts = []
-
-  def initialize(args)
-    @name = args[:name]
-    @content = args[:content]
-    Posts << self
-    #binding.pry
-  end
-
-  def self.all
-    Posts
-  end
-
 end #ends Post model
