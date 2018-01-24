5fb48ff600e111e0d77ca959abdf093b91dd151c
Brittany Morris <brittmmendez@gmail.com>
2018-01-24 16:58:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/posts' do
-    @post = Post.create(params)
+    #@post = Post.create(params)
+    @post = Post.create(name: params[:name], content: params[:content])
     redirect to '/posts'
   end
 
