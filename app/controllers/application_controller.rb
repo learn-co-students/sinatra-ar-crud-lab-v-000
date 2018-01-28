fb799ac939b928b84968c1706f889975512f62d2
churchwerks <steven.lennis@icloud.com>
2018-01-27 18:31:59 -0600

    first 5 tests passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,30 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+# shows index page
+  get '/' do
+    erb :index
   end
+
+# shows new page which has a form to create a post
+  get '/posts/new' do
+    erb :new
+  end
+
+#creates a post
+  post '/posts' do
+    @post = Post.create(params)
+    redirect to '/posts'
+  end
+
+#shows all posts on the index page
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+#shows all posts on the index page
+    get '/posts/:id' do
+      erb :show
+    end
 end
