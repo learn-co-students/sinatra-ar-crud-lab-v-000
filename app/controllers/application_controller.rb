2972da139f5708b7484f28b3f5eee7880776996e
PJ Frias <peejfrias@gmail.com>
2018-02-03 02:25:49 +0000

    SAVE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,55 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do 
-    
+    "WORKING ON IT!"
   end
+  
+  get '/posts/new' do
+    erb :new
+  end 
+  
+  post '/posts' do
+    post = Post.new
+    post.name = params["name"]
+    post.content = params["content"]
+    post.save
+    
+    @posts = Post.all
+    
+    erb :index
+  end
+  
+  get '/posts' do
+    @posts = Post.all 
+    
+    erb :index
+  end
+  
+  get '/posts/:id' do 
+    @post = Post.find(params[:id])
+    
+    erb :show 
+  end
+  
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.update(:name => params[:name], :content => params[:content])
+    
+    erb :show
+  end
+  
+  get '/posts/:id/edit' do 
+    @post = Post.find(params[:id])
+    
+    erb :edit
+  end
+  
+   delete '/posts/:id/delete' do 
+    post = Post.find(params[:id])
+    post.destroy
+    
+    erb :delete
+  end
+  
+  
 end
