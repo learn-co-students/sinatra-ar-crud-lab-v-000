fe10435d82c90b79f168242c9cc52fe0029021ba
github username <github email address>
2018-02-17 16:43:22 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,25 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+    erb :index
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  get '/posts/:id' do
+
+  end
+  
+  # post '/posts/new' do
+  #
+  # end
+  #
+  # post '/posts/:id' do
+  #
+  # end
+
+
 end
