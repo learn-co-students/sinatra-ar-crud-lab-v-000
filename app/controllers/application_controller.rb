c92fe3249bebde35c5adb94dec1da3f0ab7d4728
Chad Montoya <chadmontoya21@yahoo.com>
2018-02-20 23:45:39 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,28 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+
+  post '/posts' do
+    Post.create(name: params[:name], content: params[:content])
+    redirect '/posts'
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    binding.pry
+    erb :show
   end
 end
