0dac8dc654210ade23294f11daa7a062c8259f15
Chad Montoya <chadmontoya21@yahoo.com>
2018-02-21 05:17:55 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
 
 
   post '/posts' do
-    Post.create(name: params[:name], content: params[:content])
+    Post.create(params)
     redirect '/posts'
   end
 
