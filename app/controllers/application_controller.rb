f714b1bc960bdab1107ece5aecbdbb48be4a43a4
Sam Kalum <stkalum@gmail.com>
2018-03-01 17:02:07 -0500

    Write through /post/:id in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,29 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    Post.create(params)
+
+    redirect to('/posts')
+  end
+
+  gets '/posts' do
+    @posts = Post.all
+
+    erb :index
+  end
+
+  gets '/posts/:id' do
+    @post = Post.find_by(id: params[:id])
+
+    erb :show
   end
 end
