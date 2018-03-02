51b25c3454e2d7db86432594ece666806fe0ee17
Katrina Brinson <kbrinso@gmail.com>
2018-03-02 18:48:15 +0000

    create and read complete starting update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,34 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
   end
 
   get '/posts/new' do
     erb :new
   end
 
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
   post '/posts' do
+    newpost = Post.create(name: params[:name], content: params[:content])
+    newpost.save
+    erb :index
+  end
+
+  patch '/posts/:id' do
     erb :posts
   end
   #The controller action should use the Create CRUD action to create the blog post and save it to the database. Then, the action uses erb to render the index view page.
