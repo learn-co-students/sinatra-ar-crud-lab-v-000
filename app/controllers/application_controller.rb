b445d5f4e25d848ed8313376c766a760c6fb27e4
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-03-09 19:16:45 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,4 +25,30 @@ class ApplicationController < Sinatra::Base
     @posts = Post.all
     erb :index
   end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :'show'
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do  #updates a post
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do #delete action
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :deleted
+  end
+
+
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,19 +1,1 @@
 #Placeholder for a model
-
-class Post < ActiveRecord::Base
-
-  attr_accessor :name, :content
-
-  @@all = []
-
-  def create(data)
-    binding.pry
-    @name = data[:name]
-    @content = data[:content]
-    @@all << self
-  end
-
-  def all
-    @@all
-  end
-end
