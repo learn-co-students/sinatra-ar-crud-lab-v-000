555b442a1ef02bcc1f44335e3e2a17fc6d4ec17b
github username <github email address>
2018-03-10 18:39:51 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,43 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+    redirect to("/posts")
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  post '/posts' do
+    Post.create(params)
+    redirect to("/posts")
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    Post.find(params[:id]).update({:name => params[:name], :content => params[:content]})
+    redirect to("/posts/#{params[:id]}")
+  end
+
+  delete '/posts/:id/delete' do
+    @deleted_post = Post.find(params[:id])
+    Post.delete(params[:id])
+    erb :delete
+  end
+
 end
