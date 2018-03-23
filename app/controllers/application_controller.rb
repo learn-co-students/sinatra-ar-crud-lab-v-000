9efd5aeb52e9011e81a50fd0488c1736869e14d5
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-23 14:09:34 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,45 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :new
+  end
+  
+  get '/posts/new' do
 
+    erb :new
+  end
+
+  post '/posts' do
+    @post = Post.create(name: params[:name], content: params[:content])
+    erb :display
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :display
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    name = params[:name]
+    content = params[:content]
+    @post = Post.find(params[:id])
+    @post.update({:name => name, :content => content})
+    erb :display
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.destroy
+    erb :new
   end
 end
