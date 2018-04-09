3b9e4233ee6565550297f318283598f14c450d61
Kelsey Fecho <kfecho30@gmail.com>
2018-04-09 15:57:09 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -9,6 +10,26 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to '/posts'
+  end
 
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    Post.create(params)
+    erb :index
+    redirect to '/posts'
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
   end
 end
