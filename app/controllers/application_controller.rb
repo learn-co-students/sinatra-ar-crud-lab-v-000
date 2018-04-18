8dd6be3505155e05bddb3d12e764f9a207312fc3
Ariana Berger <ariana.berger@gmail.com>
2018-04-18 22:28:53 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,28 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
+    erb :index
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    #get info from params to post to index / create a new instance of post class!
+    erb :index
+  end
+
+  get '/posts' do
+    #@posts will get all of the posts
+    erb :index
+  end
+
+  get '/posts/:id' do
+    # This action should use Active Record to grab the post with the id that is in
+    # the params and set it equal to @post.
+    erb :show
+  end
+
+
 end
