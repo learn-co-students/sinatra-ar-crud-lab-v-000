66c44faa4ead8c08518c7d7af2faf96b3e35d089
Ariana Berger <ariana.berger@gmail.com>
2018-04-20 00:39:09 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,6 +8,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    @posts = Post.all
     erb :index
   end
 
@@ -17,11 +17,15 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/posts' do
+    @post = Post.create(params)
     #get info from params to post to index / create a new instance of post class!
-    erb :index
+    #why is Post class not being used here?
+    # erb :index
+    redirect to '/posts'
   end
 
   get '/posts' do
+    @posts = Post.all
     #@posts will get all of the posts
     erb :index
   end
@@ -29,8 +33,30 @@ class ApplicationController < Sinatra::Base
   get '/posts/:id' do
     # This action should use Active Record to grab the post with the id that is in
     # the params and set it equal to @post.
+    # binding.pry
+
+    @post = Post.find_by_id(params[:id])
     erb :show
   end
 
+  get '/posts/:id/edit' do
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+
+  end
+
+  patch 'posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :deleted
+  end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,19 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
-
-  attr_accessor :name, :content
-
-  def initialize(
+  # ALREADY DONE FOR ME BY AR
+  
+  # attr_accessor :name, :content
+  #
+  # POSTS = []
+  #
+  # def initialize(params)
+  #   @name = params[:name]
+  #   @contents = params[:content]
+  #   POSTS << self
+  # end
+  #
+  # def all
+  #   POSTS
+  # end
 end
