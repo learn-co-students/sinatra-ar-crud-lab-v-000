174d54638db088d6a109a9d8971ee7b7b9c88a56
Jonathan Tiede <jon@Jonathans-MacBook-Pro.local>
2018-05-01 14:04:14 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,52 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+  	"Test Page	"
+  end
+
+  get '/posts/new' do
+	erb :new
+  end
+
+  
+
+  post '/posts' do
+  	Post.create(:name => params[:name], :content => params[:content])
+  	@posts = Post.all 
+  	erb :index
+  end
+
+  get '/posts' do
+  	@posts = Post.all
+	erb :index
+  end
+	  
+
+  get '/posts/:id' do
+  	@post = Post.find_by(id: params[:id])
+
+  	erb :show
+  end
+
+  get '/posts/:id/edit' do 
+  	@post = Post.find_by(id: params[:id])
+
+  	erb :edit
+  end
+
+  patch '/posts/:id' do
+  	@post = Post.find_by(id: params[:id])
+  	@post.name = params[:name]
+  	@post.content = params[:content]
+  	@post.save
+  	erb :show
 
   end
+
+  delete '/posts/:id/delete' do
+  	@post = Post.find_by(id: params[:id])
+  	@post.delete
+
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,18 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+
+	# attr_accessor :name, :content
+
+	# @@all = []
+
+	# def initialize(params)
+	# 	@name = params[:name]
+	# 	@content = params[:content]
+	# 	@@all << self
+	# end
+
+	# def self.all 
+	# 	@@all 
+	# end
 end
