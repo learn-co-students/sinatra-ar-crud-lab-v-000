783df536ea087342dcf6a3c76a5082405e731040
Aspen James <aspenjames@tqca.org>
2018-05-03 14:51:16 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,44 @@ class ApplicationController < Sinatra::Base
   get '/' do
 
   end
+
+  get '/posts/new'do
+    erb :new
+  end
+
+  post '/posts' do
+    Post.create(:name => params["name"], :content => params["content"])
+    redirect '/posts'
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  patch '/posts/:id' do
+    post = Post.find(params[:id])
+    post.name = params["name"] unless params["name"].nil?
+    post.content = params["content"] unless params["content"].nil?
+    post.save
+    redirect "/posts/#{params[:id]}"
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.destroy
+    erb :delete
+  end
+
+
 end
