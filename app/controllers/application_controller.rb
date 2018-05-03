0a7c91fbac48841d09e6281512cfc1532e5f9e29
kristinreddington <kristinreddington@gmail.com>
2018-05-03 16:02:11 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,45 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-
+  configure :development do
+      set :database,
+      "sqlite3:db/posts.db"
+    end
+    
+  get '/posts/new' do
+    erb :new
   end
+
+  post '/posts' do
+  attributes= {name: params[:name], content: params[:content]}
+  Post.create(attributes)
+  erb :index
+end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit'do
+  @post = Post.find(params[:id])
+  erb :edit
+  end
+
+  patch '/posts/:id' do
+     Post.update(params[:id], name: params[:name], content: params[:content])
+     erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.delete
+    erb :show
+  end
+
 end
