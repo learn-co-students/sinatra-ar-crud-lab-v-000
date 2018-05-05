760543613635c77e3c0ae80e73fc2396bb20c664
Larry <github email address>
2018-05-04 20:06:22 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -9,6 +10,47 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
+    "Rickroll"
   end
+  
+  get '/posts/new' do
+    erb :new
+  end
+  
+  post '/posts' do
+    new_post = Post.new(name: params[:name], content: params[:content])
+    new_post.save
+    @posts = Post.all
+    erb :index
+  end
+  
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+  
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+  
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+  
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    redirect to "/posts/#{params[:id]}"
+  end
+  
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.delete
+    erb :delete
+  end
+  
 end
