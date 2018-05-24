86746350b14a97493685a9cc00507ad37704c24d
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-24 18:22:36 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,12 +24,26 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/:id' do
-    @post_id = Post.find_by_id(params[:id])
+    @post = Post.find_by_id(params[:id])
     erb :show
   end
 
   get '/posts/:id/edit' do
-    @post_edit = Post.find_by_id(:id)
+    @post_edit = Post.find_by_id(params[:id])
     erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do #delete action
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :deleted
   end
 end
