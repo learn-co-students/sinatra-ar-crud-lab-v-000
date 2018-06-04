1bdbe4a4e615598f066cdaaa35fe6ea875507cf2
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-06-03 23:02:35 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,43 +14,40 @@ class ApplicationController < Sinatra::Base
     erb :'posts/index'
   end
 
-              # create form
-              get '/posts/new' do
-                erb :'posts/new'
-              end
+  # create form
+  get '/posts/new' do
+    erb :'posts/new'
+  end
 
-              # create
-              post '/posts' do
-                @post = Post.create(params)
-                erb :"posts/show"
-              end
+  # create
+  post '/posts' do
+    Post.create(params)
+    redirect to '/posts'
+  end
 
-              # read
-              get '/posts/:id' do
-                @post = Post.find(params[:id])
-                erb :'posts/show'
-              end
+  # read
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :'posts/show'
+  end
 
-              # update form
-              get '/posts/:id/edit' do
-                # edit form displayd here
-                @post = Post.find(params[:id])
-                erb :'posts/edit'
-              end
+  # update form
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :'posts/edit'
+  end
 
-              # update
-              patch '/posts/:id' do
-                @post = Post.find(params[:id])
-                @post.name = params[:name]
-                @post.content = params[:content]
-                @post.save
-                erb :'posts/show'
-              end
-    
+  # update
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :'posts/show'
+  end
 
   # delete
   delete '/posts/:id/delete' do
-    # delete post here
     Post.find(params["id"]).delete
     @posts = Post.all
     erb :'posts/index'
