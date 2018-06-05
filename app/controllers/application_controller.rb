ecd7d8937c2bc901fc780fc85fc55344849c63b2
Ally Netzel <netzelally@gmail.com>
2018-06-05 18:01:10 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,12 +13,31 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
-  post '/posts' do
-    #binding.pry
-    @post1 = Post.create(name: params[:name], content: params[:content])
-
+  get '/posts' do
+    @posts = Post.all
     erb :index
   end
 
+  post '/posts' do
+    #binding.pry
+    Post.create(name: params[:name], content: params[:content])
+    redirect to '/posts'
+  end
+
+  get '/posts/:id' do
+    @post = Post.find_by(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find_by(params[:id])
+
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find_by(params[:id])
+    erb :edit
+  end
 
 end
