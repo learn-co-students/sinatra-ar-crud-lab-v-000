884ed094266a0153c9644f3df392a688ff1c704f
eyi1 <ej7729@gmail.com>
2018-06-05 16:20:10 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-
+  get '/posts/new' do #creates new form
+    erb :new
   end
+
+  post '/posts' do #filled-in form
+    @post = Post.create(params) #{name => "",}
+    redirect to '/posts'
+  end
+  
+  get '/posts' do #loads index - shows all posts created
+    @posts = Post.all
+    erb :index
+  end
+
+ get '/posts/:id' do 
+    @single_post = Post.find_by_id(params[:id])
+    erb :show
+ end
+
+ get '/posts/:id/edit' do 
+    @single_post = Post.find_by_id(params[:id])
+    erb :edit
+ end
+
+ patch '/posts/:id' do 
+    @single_post = Post.find_by_id(params[:id])
+    @single_post.name = params[:name]
+    @single_post.content = params[:content]
+    @single_post.save
+    erb :show
+ end
+
+ delete '/posts/:id/delete' do
+    @single_post = Post.find_by_id(params[:id])
+    @single_post.delete
+    erb :deleted
+ end
+
 end
