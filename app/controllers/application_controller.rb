29d4274b211e6c375acf4d4fece3bb161444b4f5
Nicholas Bharucha <nickbharucha@ou.edu>
2018-06-07 23:06:53 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,19 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-
+  get '/posts/new' do
+    erb :new
   end
+
+  post '/posts' do
+    @post = Post.create(name: params[:name], content: params[:content])
+    @post.save
+    erb :index
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
 end
