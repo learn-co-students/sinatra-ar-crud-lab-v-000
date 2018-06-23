6dc62f6071fe8cd643a556c6565f9387293ce594
ho-tonym <ho.tonym@gmail.com>
2018-06-22 20:44:26 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,22 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+
+  # attr_accessor :name, :content
+  # @@all = []
+  #
+  # def initialize(params)
+  #   @name = params[:name]
+  #   @name = params[:content]
+  #   @@all << self
+  # end
+  #
+  # def self.all
+  #   @@all
+  # end
+  #
+  # def self.create
+  #   post = self.create(params)
+  # end
+
 end
