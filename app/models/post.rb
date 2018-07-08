1226f9c0dc8b7a23f83ba1c3dd027c5f27a82de3
Pamela Frieze <pfrieze@gmail.com>
2018-07-08 18:58:40 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+  #attr_accessor :name, :content
+
+
+  #def initialize(arg)
+  #  @name = arg[:name]
+  #  @content = arg[:content]
+  #end
+
 end
