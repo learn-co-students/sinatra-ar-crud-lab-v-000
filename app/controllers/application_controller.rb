764a801f049e9eb88de0588b7669c77e36afb80e
Cristina Lugo <cmlugoce@syr.edu>
2018-07-15 02:28:29 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,28 @@ class ApplicationController < Sinatra::Base
   get '/' do
 
   end
+  
+ get '/posts/new' do
+    erb :new
+  end
+  
+  get '/posts' do 
+    @posts = Post.all
+    erb :index
+  end
+  
+  get '/posts/:id' do 
+    @posts = Post.find_by_id(params[:id])
+    erb :show
+  end 
+  
+  get '/posts/:id/edit' do 
+    @post = Post.find_by_id(params[:id])
+    erb :edit
 end
+  delete 'posts/:id/delete' do 
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :delete
+  end 
+end 
