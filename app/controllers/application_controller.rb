cbf392cc555415876c30c14903756f5499a9d504
Adam Kalt <adam.kalt.1@gmail.com>
2018-07-17 23:29:57 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,9 +23,14 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/posts:id' do
+  get '/posts/:id' do
     @post = Post.find_by(id: params[:id])
     erb :show
   end
+
+get '/posts/:id/edit' do
+  @post = Post.find_by(id: params[:id])
+  erb :edit
+end
 
 end
