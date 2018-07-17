97875cffbd291a968c988cf44c4f65bcf3cc480b
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-07-17 00:06:29 -0400

    Finish route and view for edit
    Modified the name attributes in the new view to properly create posts, as a hash isnt needed when its always for the same object.
    Starting to work through delete route and view, moved comments about delete button to show since its going to be added there.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,39 +27,34 @@ class ApplicationController < Sinatra::Base
   get '/posts' do
     @all = Post.all
 
-    # shows all posts title and content
     erb :index
   end
 
   get '/posts/:id' do
     @post = Post.find_by_id(params[:id])
 
-    # displays posts title and content
     erb :show
   end
 
   get '/posts/:id/edit' do
-=begin
-  - should contain a form to update a specific blog post and POSTs to a controller action, patch '/posts/:id'
-  - should include <input id="hidden" type="hidden" name="_method" value="patch">
-  - add the use Rack::MethodOverride to your config.ru file so that your app will know how to handle patch and delete
-=end
+    @post = Post.find_by_id(params[:id])
 
     erb :edit
   end
 
-  delete '/posts/:id/delete' do
-=begin
-  - add delete button to show page that's actually a form
-  - it will send a post requre to this delete controller action
-  - after the deletion render the view below which confirms deletion
-  - make a form that has no input fields, only a "submit" button with a value of "delete"
-  - give your form a method of "post" and an action of "/posts/:id/delete'
-  - Make sure to dynamically set the :id of the form action
-  - be sure the form includes the hidden input tag to change the request from post to delete
-=end
+  patch '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
 
-    erb :delete
+    redirect to "/posts/#{@post.id}"
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    redirect to '/posts'
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,29 +1,4 @@
 
-# these methods should all be default to ActiveRecord
-
 class Post < ActiveRecord::Base
-
-=begin
-  attr_accessor :name, :content
-  POSTS = []
-
-  def initialize(name, content)
-    self.name = name
-    self.content = content
-  end
-
-  def self.create(name:, content:)
-    POSTS << new(name, content)
-    last
-  end
-
-  def self.all
-    POSTS
-  end
-
-  def self.last
-    all[-1]
-  end
-=end
 
 end
