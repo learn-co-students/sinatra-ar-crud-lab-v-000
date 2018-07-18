d03bce4d7c05e549d36791f31d38b5d4dd690ff3
Brett Weisberg <weisberg.brett@gmail.com>
2018-07-18 23:36:49 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < Sinatra::Base
 
   post '/posts' do
     @post = Post.create(name: params[:name], content: params[:content])
+    binding.pry
     erb :'posts'
   end
 
