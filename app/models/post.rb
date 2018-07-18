587d8c840b1bc3135a4e1eca350b5f8f78eae2d5
Brett Weisberg <weisberg.brett@gmail.com>
2018-07-18 02:00:46 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,6 @@
-#Placeholder for a model
-
 class Post < ActiveRecord::Base
 
   attr_accessor :name, :content
 
-  def initialize(name, content)
-    @name = name
-    @content = content
-  end
 
 end
