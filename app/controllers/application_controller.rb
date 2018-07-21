5df56b327ae356b9e0fc781918709380383d7172
Kachel Wittig <kjwittig@gmail.com>
2018-07-21 00:59:19 -0500

    reached a stopping point

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -8,7 +7,37 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-
+  get '/posts/new' do
+    erb :new
   end
+
+  post '/posts' do
+    Post.create(params)
+    erb :index
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find_by(id: params[:id])
+
+    erb :edit
+  end
+
+  patch '/models/:id' do
+    binding.pry
+        # @post.name = params["name"]
+        # @post.content = params["content"]
+        # @post.save
+  end
+
+
 end
