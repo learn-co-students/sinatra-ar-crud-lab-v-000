5ed826266ec091f8f1f2bbbe062de0f99b1bc731
treverandersen <trever.r.andersen@gmail.com>
2018-08-02 17:31:23 -0500

    setup controller action for POST to create a post, setup get request for posts to show all posts, created index.erb view file to load posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,16 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-
-  end
-  
   get '/posts/new' do
     erb :'new'
   end
 
+  post '/posts' do 
+    @post = Post.create(params)
+    redirect to '/posts'
+  end
+
+  get '/posts' do
+    @posts = Post.all 
+    erb :'index'
 end
