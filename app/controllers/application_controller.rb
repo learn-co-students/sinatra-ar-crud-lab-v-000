de77fbd1d003948fe587ee3f38dfae6643be58d6
Emily S <emily.sugano@gmail.com>
2018-08-13 22:11:58 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,8 +26,28 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/show' do
+  get '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
     erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :delete
   end
 
 end
