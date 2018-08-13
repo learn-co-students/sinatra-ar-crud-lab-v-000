2b439d79885f02d982f3d128e62bc0df15a3e8e8
Joana Palero <joana.palero24@gmail.com>
2018-08-13 21:15:41 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 
 require_relative '../../config/environment'
 
+require 'pry'
+
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -8,7 +10,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  # get '/' do
+  # end
 
+  get '/posts/new' do
+    erb :new
+  end
+
+   post '/posts' do
+    @post =  Post.create(params)
+    erb :index
+  end
+
+   get '/posts' do
+    erb :index
+  end
+
+   get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+   get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+   patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.update(name: params[:name], content: params[:content])
+    erb :show
+  end
+
+   delete '/posts/:id/delete' do
+
+    @post = Post.find(params[:id])
+    @post.destroy
+    erb :delete
   end
 end
