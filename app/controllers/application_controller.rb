9a7ac575acde950c72151d19af0cd6db9e2d68a3
BrittanyTinnin <bntinnin7@gmail.com>
2018-08-15 11:41:58 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,29 @@ class ApplicationController < Sinatra::Base
   get '/' do
 
   end
+
+  get '/posts/new' do
+
+    
+    erb :new
+  end
+
+  post '/posts' do
+    Post.create(name: params[:name], content: params[:content])
+    
+    redirect '/posts'
+  end
+
+  get '/posts' do
+    @posts = Post.all
+
+    erb :index
+  end
+
+  get '/posts/:id' do
+    
+    @post = Post.find_by(id: params[:id])
+
+    erb :show
+  end
 end
