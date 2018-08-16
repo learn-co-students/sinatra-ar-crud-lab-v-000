f96f5d53f69b64dd2d91c9bf6ef276edc5341fe4
BrittanyTinnin <bntinnin7@gmail.com>
2018-08-16 11:25:31 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,4 +52,11 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  delete '/posts/:id/delete' do
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+
+    erb :delete
+  end
+
 end
