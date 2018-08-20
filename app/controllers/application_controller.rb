8315e76d97f8bbc5ff812dcc06ee35ae0596f045
James Lotruglio <jimlotruglio@yahoo.com>
2018-08-20 01:45:41 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -9,6 +10,58 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect '/posts/new'
 
   end
+
+  get '/posts' do
+    @post = Post.all
+    erb :posts
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+
+    #{"name"=>"jim", "content"=>"this ", "submit"=>"submit"}
+    @post = Post.new
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    post_id = @post.id
+    redirect '/posts'
+  end
+
+
+
+  get '/posts/:id' do
+
+    @post = Post.find_by_id(params[:id])
+   erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+
+patch '/posts/:id' do
+  @post = Post.find_by_id(params[:id])
+  @post.name = params[:name]
+  @post.content = params[:content]
+  @post.save
+  redirect "/posts/#{@post.id}"
+end
+
+delete '/posts/:id' do
+  @post = Post.find_by_id(params[:id])
+  @post.destroy
+  redirect '/posts'
+end
+
+
+
 end
