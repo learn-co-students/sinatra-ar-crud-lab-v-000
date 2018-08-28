b01572d64ed6fa3abeff3e652321d0b56faba6eb
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-08-28 00:19:38 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,41 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/posts/new' do # loads new blog post form
+    erb :new
+    end
 
-  end
-end
+    post '/posts' do #creates a blog post
+      @post = Post.create(params)
+      redirect to '/posts'
+    end
+
+    get '/posts' do #loads index page with listing of all blog posts
+       @posts = Post.all
+       erb :index
+     end
+
+      get '/posts/:id' do #loads show page with listing of selected blog post
+       @post = Post.find_by_id(params[:id])
+       erb :show
+     end
+
+    get '/posts/:id/edit' do #loads edit blog post form with prefilled listing of selected blog post
+       @post = Post.find_by_id(params[:id])
+       erb :edit
+     end
+
+    patch '/posts/:id' do #updates a selected blog post
+       @post = Post.find_by_id(params[:id])
+       @post.name = params[:name]
+       @post.content = params[:content]
+       @post.save
+       erb :show
+  	  end
+
+      delete '/posts/:id/delete' do #deletes a selected blog post
+       @post = Post.find_by_id(params[:id])
+       @post.delete
+       redirect to '/posts'
+     end
+     end
