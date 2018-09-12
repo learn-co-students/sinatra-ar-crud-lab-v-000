7840901a48bdc9c03c989cb9be1eb1fc00a6c95c
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-12 18:21:18 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,38 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/posts/new' do
+    erb :new
+  end
 
+  post '/posts' do
+    Post.create(:name => params[:name], :content => params[:content])
+  end
+
+  get '/posts' do
+    @posts = []
+    Post.all.each {|post| @posts << post }
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.update(params[:content])
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.destroy
+    erb :delete
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,16 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+  # belongs_to :user
+
+  attr_accessor :name, :content
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @content = params[:content]
+    @@all << self
+  end
+
 end
