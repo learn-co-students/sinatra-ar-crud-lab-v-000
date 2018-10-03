7a0e12b97fd247abb719689a04d75c5925608fb4
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-03 16:54:05 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -8,7 +9,43 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-
+  get '/posts/new' do
+    erb :new
+    
+  end
+  
+  post '/posts' do
+    @posts = Post.create(params)
+    redirect_to '/posts' 
+    #https://apidock.com/rails/ActionController/Base/redirect_to
+  end
+  
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+  
+  get '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
+  
+  get '/posts/:id/edit' do
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+  
+  patch '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+  
+  delete '/posts/:id/delete' do
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :delete
   end
 end
