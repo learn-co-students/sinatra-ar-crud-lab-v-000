34c1fb9cb61ae7ede3b7192d8d283bba2eb92bc2
Christia T <christiatidwell@bellsouth.net>
2018-10-14 18:51:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,15 +8,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-
-  end
-
-  get '/posts' do
-    @posts = Post.all
-    erb :index
-  end
-
+  
   get '/posts/new' do
     erb :new
   end
