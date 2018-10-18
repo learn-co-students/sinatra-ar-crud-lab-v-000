cfc39515491cc12e9eb03df35ad13028ab0b4c99
Srija S <srijakumar7@gmail.com>
2018-10-18 12:07:50 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,11 +33,23 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  get '/posts/:id/edit' do 
+  get '/posts/:id/edit' do
      @post = Post.find_by_id(params[:id])
      erb :edit
    end
 
+   patch '/posts/:id' do
+      @post = Post.find_by_id(params[:id])
+      @post.name = params[:name]
+      @post.content = params[:content]
+      @post.save
+      erb :show
+    end
 
+delete '/posts/:id/delete' do
+  @post = Post.find_by_id(params[:id])
+   @post.delete
+   erb :deleted
+end
 
 end
