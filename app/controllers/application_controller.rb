334d10aa2d1534d841ef6f9179261f323624615d
Srija S <srijakumar7@gmail.com>
2018-10-18 11:56:52 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
-require_relative '../../config/environment'
+require './config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -9,6 +10,30 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
+    "hello"
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+  #  binding.pry
+    @post=Post.create(name: params[:name], content: params[:content])
+    erb :index
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get 'posts/:id' do
+    @post = Post.find_by_id(param[:id])
+    erb :show
+  end
+
+
+
+
 end
