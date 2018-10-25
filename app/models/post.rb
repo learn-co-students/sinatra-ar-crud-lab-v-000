fe6b9fb4ab2526e83d4bb8f6c58b5a430a162aec
Laura Darlak <laura@polyporedesign.com>
2018-10-25 18:43:50 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+  # attr_reader :name, :content
+  # 
+  # POSTS = []
+  # 
+  # def initialize(name, content)
+  #   @name = name
+  #   @content = content
+  #   POSTS << self
+  # end
+
 end
