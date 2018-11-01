5fe1e24b01cc862d8db59d2bdbb53d371d58ed39
Brandon Green <brandon@brandonleeproductions.com>
2018-11-01 02:31:39 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,21 +8,25 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/posts/new' do
+  get '/posts' do #loads index page
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/new' do #loads new form
     erb :new
   end
 
-  post '/posts' do
-    @posts = Post.all
-
-    erb :index
-
-    end
-
-    get '/posts/:id' do
-      @posts = Post.find(params[:id])
-      erb :show
-    end
+  post '/posts'do #creates a post
+    @post = Post.create(params)
+    redirect to "/posts/#{@post.id}"
+    #params hash looks like this>>> {"name"=>"name of post", "content"=>"content for post"}
   end
 
+####START AT 30 MIN MARK IN VIDEO LECTURE#####
+
+  get '/posts/:id' do #loads show page
+    @post = Post.find(params[:id])
+    erb :show
   end
+end
