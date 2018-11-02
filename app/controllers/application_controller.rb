bfb83a7b99586d8fdae5c0caf2d97b0e6340957f
Brandon Green <brandon@brandonleeproductions.com>
2018-11-02 02:24:16 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,26 +7,47 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  get '/posts/new' do #loads new form
+    erb :new
+  end
 
   get '/posts' do #loads index page
     @posts = Post.all
     erb :index
   end
 
-  get '/posts/new' do #loads new form
-    erb :new
-  end
 
   post '/posts'do #creates a post
     @post = Post.create(params)
-    redirect to "/posts/#{@post.id}"
+    redirect to "/posts"
     #params hash looks like this>>> {"name"=>"name of post", "content"=>"content for post"}
-  end
 
-####START AT 30 MIN MARK IN VIDEO LECTURE#####
+  end
 
   get '/posts/:id' do #loads show page
     @post = Post.find(params[:id])
     erb :show
+  end
+
+  get '/posts/:id/edit' do #find a post by id and edit
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+#####RESUME VIDEO AT 1:04:01#######
+
+  patch '/posts/:id' do #updates a post and then shows it
+    @post = Post.find(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+
+    redirect "/posts/#{@post.id}"
+  end
+
+  delete '/posts/:id' do #delete post by id
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect '/posts'
   end
 end
