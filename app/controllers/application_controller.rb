b3bd081dba736fe4b46accac4b0995696ae82e07
Brian Birdwell <bbirdwell@gmail.com>
2018-11-04 18:02:32 +0000

    set up first few pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -8,7 +9,28 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/posts/new' do
 
+    erb :new
+  end
+
+  post '/posts' do
+    @post = Post.new(params[:post])
+    @posts = Post.all
+    # binding.pry
+    erb :index
+  end
+
+  get '/posts' do
+    # get ALL posts and render on index via @posts
+    @posts = Post.all
+
+    erb :index
+  end
+
+  get '/posts/:id' do
+    # find a post and display it
+
+    erb :show
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,18 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+  attr_accessor :name, :content
+
+  POSTS = []
+
+  def initalize(details)
+    @name = details[:name]
+    @content = details[:content]
+    POSTS << self
+  end
+
+  def self.all
+    POSTS
+  end
+
 end
