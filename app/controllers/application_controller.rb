06642bd590b39fe852728aad493d5738b4c961b5
Barbara Choy <barbchoy@gmail.com>
2018-11-09 21:52:21 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,8 +32,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/:id/edit' do
-    @post = Post.find_by(params[:id])
     binding.pry
+    @post = Post.find_by(params[:id])
     erb :edit
   end
 
