9355bef1fb2dd0c28d8f52b295c8e5f29c182839
Harley Rayner <harleymteden@gmail.com>
2018-11-28 20:34:19 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,15 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :index
+  end
 
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    erb :index
   end
 
 
