b108f49ad71f798693244005fc33862c79e80919
Rima Jean <rima.jean@gmail.com>
2018-11-29 22:45:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,21 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
 
+
+  patch '/posts/:id' do  #updates a post
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
   end
+
+
+  delete '/posts/:id/delete' do #delete action
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    redirect to '/posts'
+  end
+
+
 end
