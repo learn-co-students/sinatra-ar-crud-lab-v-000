4b4490300ec53e00b4934cc2f9314c178483d8d1
Naudria <rima.jean@gmail.com>
2018-11-30 11:12:59 -0600

    grab all of the articles and store them in an instance variable, @articles.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,15 @@ class ApplicationController < Sinatra::Base
   	erb :new
   end
 
+  post '/articles' do
+  	Article.create(params)
+  	redirect '/articles'
+  end 
+
+  get '/articles' do
+  	@articles = Article.all
+
+  	erb :index
+  end
+
 end
