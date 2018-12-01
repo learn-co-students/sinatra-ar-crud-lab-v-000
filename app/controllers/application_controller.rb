bbf320349551ce3c41738753cb7bb0fce54fe137
Naudria <rima.jean@gmail.com>
2018-12-01 17:52:04 -0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,31 @@ class ApplicationController < Sinatra::Base
   get '/' do
   end
 
+  get '/articles/new' do
+    erb :new
+  end
 
+  post '/articles' do
+    Article.create(params)
+    redirect '/articles'
+  end
+
+  get '/articles' do
+    @article = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params["id"])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params["id"])
+    erb :edit
+  end
+  
+  
   patch '/articles/:id' do  #updates a post
     @article = Post.find_by_id(params[:id])
     @article.name = params[:name]
