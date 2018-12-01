711fc26caba68bc466d0d5e663094b0ab3df7669
Naudria <rima.jean@gmail.com>
2018-12-01 17:58:26 -0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/articles' do
-    @article = Article.all
+    @articles = Article.all
     erb :index
   end
 
@@ -36,20 +36,25 @@ class ApplicationController < Sinatra::Base
   end
   
   
-  patch '/articles/:id' do  #updates a post
-    @article = Post.find_by_id(params[:id])
-    @article.name = params[:name]
-    @article.content = params[:content]
-    @article.save
-    erb :show
+  patch '/articles/:id' do
+    id = params["id"]
+    new_params = {}
+    old_article = Article.find(id)
+    new_params[:name] = params["name"]
+    new_params[:content] = params["content"]
+    old_article.update(new_params)
+
+    redirect "/articles/#{id}"
   end
 
 
-  delete '/articles/:id/delete' do #delete action
-    @article = Article.find_by_id(params[:id])
-    @article.delete
-    redirect to '/articles'
+
+  delete '/articles/:id/delete' do
+    @article = Article.find(params["id"])
+    @article.destroy
+    erb :delete
   end
+
 
 
 end
