8e6954d7d13b3ac4eeebdc36b4c601256e0581cd
Rima Jean <rima.jean@gmail.com>
2018-12-01 18:37:04 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,22 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+
+  patch '/articles/:id' do  #updates a post
+    @article = Post.find_by_id(params[:id])
+    @article.name = params[:name]
+    @article.content = params[:content]
+    @article.save
+    erb :show
+  end
+
+
+  delete '/articles/:id/delete' do #delete action
+    @article = Article.find_by_id(params[:id])
+    @article.delete
+    redirect to '/articles'
+  end
+
+
 end
