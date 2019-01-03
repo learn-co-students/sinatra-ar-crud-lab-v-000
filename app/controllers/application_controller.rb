f0b4ff5985cae2c58643dfc4072e54c4fbc94593
Erin Green <erin.y.green@gmail.com>
2019-01-03 21:09:10 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,18 +9,47 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles"
   end
   
+  #create 
   get '/articles/new' do 
+    @article = Article.new 
     erb :new 
   end
   
   post '/articles' do
-    erb :index
+    @article = Article.create(params)
+    redirect to "/articles/#{@article.id}"
   end
   
   get '/articles' do
-    @articles 
-    
+    @articles = Article.all 
+      
+    erb :index 
   end
+  
+  get '/articles/:id' do
+    @current_article = Article.find(params[:id])
+    erb :show 
+  end
+  
+  #update
+  get '/articles/:id/edit' do 
+    @article = Article.find(params[:id])
+    erb :edit 
+  end
+  
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{@article.id}"
+  end
+  
+  delete '/articles/:id' do 
+   Article.destroy(params[:id])
+   redirect to "/articles" 
+  end
+  
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,19 +1,17 @@
-#Placeholder for a model
-
 class Article < ActiveRecord::Base
   
-  attr_accessor :title, :content 
+  # attr_accessor :title, :content 
   
-  @@all = []
+  # @@all = []
   
-  def initialize(params)
-    @title = params[:title]
-    @content = params[:content]
-    @@all << self 
-  end
+  # def initialize(params)
+  #   @title = params[:title]
+  #   @content = params[:content]
+  #   @@all << self 
+  # end
   
-  def self.all 
-    @@all 
-  end
+  # def self.all 
+  #   @@all 
+  # end
   
 end
