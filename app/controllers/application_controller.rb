07f9dcb472bf8a8cda4e32c4f2de7f5720d4d2d3
Rachel Wyatt <rw126@evansville.edu>
2019-01-05 18:00:32 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,43 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+
   get '/articles/new' do
     erb :new
   end
 
   post '/articles' do
-    @article = Article.create(title: params[:name], content: params[:content])
-    erb :index
+    @article = Article.create(params)
+    redirect '/articles/3'
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by_id.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    Article.update(params)
+    redirect "/articles/:id"
+  end
+
+  delete 'articles/:id/delete' do
+    @article = Article.find(params["id"])
+    @article.destory
+    erb :delete
   end
 
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,4 @@
 
 class Article < ActiveRecord::Base
-  attr_accessor :title, :content
- 
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
-  end
+
 end
