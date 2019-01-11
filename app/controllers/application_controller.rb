043128369e2f2453b6d9f798aca6d411052390ed
Vince Andrew Pimentel <contact@vincep.me>
2019-01-11 21:33:48 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,23 @@
 
-require_relative '../../config/environment'
+require_relative "../../config/environment"
 
 class ApplicationController < Sinatra::Base
 
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :public_folder, "public"
+    set :views, "app/views"
   end
 
-  get '/' do
+  get "/" do
+  end
+
+  get "/articles/new" do
+    erb :new
+  end
+
+  post "/articles" do
+    Article.create(params)
+
+    erb :index
   end
 end
