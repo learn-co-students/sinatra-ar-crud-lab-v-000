a4730be6bb7be5222ec7f533127fa2b008046fee
Heather Miller <heather.miller114@gmail.com>
2019-01-18 20:07:30 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,12 +16,39 @@ class ApplicationController < Sinatra::Base
   end
   
   post '/articles' do
-    @article = Article.new(params[:article])
-    @articles = Article.all
-    
+    @article = Article.create(params)
+
     redirect to "/articles/#{@article.id}"
+  end
+  
+  get '/articles' do #index
+    @articles = Article.all
     erb :index
   end
   
+  get '/articles/:id' do #show
+    @article = Article.find(params[:id])
+    erb :show
+  end
+  
+  get '/articles/:id/edit' do #update
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+  
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+    redirect "/articles/#{@article.id}"
+  end
+  
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+    @article.delete
+    redirect to '/articles'
+  end
   
 end
