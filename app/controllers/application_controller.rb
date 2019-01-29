5e21332f7fd3fbc72c00e3ab1878b1b352a31fbd
dlyons8 <dlyons8@binghamton.edu>
2019-01-29 13:11:34 +0200

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,14 +13,14 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/articles/:id' do
-    @article = Article.find(params[:id].to_i)
+    @article = Article.find(params[:id])
     erb :show
   end
 
   post '/articles' do
     Article.create(params)
-    @articles = Article.all
-    erb :index
+    @article = Article.last
+    erb :show
   end
 
   get '/articles' do
@@ -29,16 +29,21 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
     erb :edit
   end
 
   patch '/articles/:id' do
-    @article = Article.find(params[:id].to_i).update(params)
+    # binding.pry
+    @article = Article.find(params[:id])
+    @article.update(:title => params["title"], :content => params["content"])
     erb :show
   end
 
   delete '/articles/:id' do
-    Article.find(params[:id].to_i).destroy
+    Article.find(params[:id]).destroy
+    @articles = Article.all
+    erb :index
   end
 
 end
