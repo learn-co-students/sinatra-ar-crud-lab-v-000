99bbce62c1d26e7bf11fd10f6fdf59ecb611d383
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-02-11 21:39:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,35 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+    #reminder that these attributes the model inherits from the table
+    #in db migrate
+    redirect to "/articles/#{@article.id}"
+    #this redirect goes to an interpolated version of /articles/:id
+    #redirecting routes is done like this
+    #" " works better than ' '
+  end
+
+  get '/articles' do
+    @articles = Article.all #inherited from ActiveRecord,
+    #the tables magically have all methods
+    erb :index
+  end
+
+  get '/articles/:id' do
+      @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    
+
+    erb :edit
+  end
 end
--- config.ru
+++ config.ru
@@ -4,6 +4,8 @@ if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
-use Rack::MethodOverride
+use Rack::MethodOverride 
+#Reminder: Remember to add the use Rack::MethodOverride to your config.ru file 
+#so that your app will know how to handle PATCH, PUT, and DELETE requests!
 
 run ApplicationController
