a785dac551a6225c7e0c61bf615cf4ed797d3677
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-03-08 18:07:30 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,17 +28,27 @@ class ApplicationController < Sinatra::Base
 
   get '/articles/:id' do
     @article = Article.find(params[:id])
+
     erb :show
   end
 
   get '/articles/:id/edit' do
+    @article2 = Article.find(params[:id])
     erb :edit
   end
 
   patch '/articles/:id' do
+    article = Article.find(params[:id])
+    article.title = params[:title]
+    article.content = params[:content]
+    article.save
+    redirect to "/articles/#{article.id}"
   end
 
   delete '/articles/:id' do
+    Article.delete(params[:id])
+
+    redirect to "/articles"
   end
 
 end
