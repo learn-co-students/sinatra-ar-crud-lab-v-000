c4f026be453286d6b63ffe59ba0c3ec43e74b78b
Katie Moosman <moosman.katie@gmail.com>
2019-03-09 03:42:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/articles' do
-    Model.create(title: params[:article_title], content: paramas[:article_content])
+    Model.create(title: params[:article_title], content: params[:article_content])
     erb :index.erb
   end
 
