73202325e08473a31afa963d9f582fbf7ebab0aa
Cheryl K <cherylkelson@icloud.com>
2019-03-22 21:14:21 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,24 +8,45 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+
   get '/articles/new' do
+    # @article = params[:title][:content]
     erb :new
   end
 
 
-  get '/articles' do
-    @articles = all_of_the_articles.select do |article|
-      article.id == params[:id]
-    end
-  erb :index
+  post '/articles/new' do
+    @article = params[:title][:content]
+    erb :new
   end
 
-
-
-  get '/articles/:id' do
-    @article = Article.find(params[:id])
-    erb :show
-
-  end
-
+#
+#   get '/articles' do
+#     @articles = all_of_the_articles.select do |article|
+#       article.id == params[:id]
+#     end
+#   erb :index
+#   end
+#
+#
+#
+#
+#   get '/articles/:id' do
+#     @article = Article.find(params[:id])
+#     erb :show
+#   end
+#
+#
+#
+#   patch '/articles/:id/edit' do
+#     @article = Article.find(params[:id])
+#     erb :edit
+#   end
+#
+#   delete '/articles/:id' do
+#     erb :show
+#   end
+#
+# # binding.pry
+#
 end
