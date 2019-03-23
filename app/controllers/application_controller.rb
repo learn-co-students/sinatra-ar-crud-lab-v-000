21e67ae622bd7f93cfe1801da73aaa51d0af6576
helloamandamurphy <helloamandamurphy@gmail.com>
2019-03-23 18:00:46 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,33 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  #redirects plain URL to show all articles
   get '/' do
+    redirect to '/articles'
   end
+
+  #view all articles
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  #page for user to add new data
+  get '/articles/new' do
+    @article = Article.new
+    erb :new
+  end
+
+  #saves user data, and shows new published post
+  post '/articles' do
+    @article = Article.create(params)
+    redirect to "/articles/#{@article.id}"
+  end
+
+  #show specific article based on article id
+  get 'articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,14 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+  #@@all = []
+
+  #def initialize(text)
+    #@article = params[:text]
+    #@@all << @article
+  #end
+
+  #def self.all
+    #@@all
+  #end
 end
