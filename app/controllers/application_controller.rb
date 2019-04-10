8e4eef6363ac0bf85410b6a7b080e60a3f0937eb
Heidi Hlavinka <hchlav@gmail.com>
2019-04-10 01:37:17 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,16 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to '/articles'
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get 'articles/new' do
+    @articles = Article.new
+    erb :new
   end
 end
