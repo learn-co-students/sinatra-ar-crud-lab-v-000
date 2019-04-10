934b8042c9c4a2cb6259a2cda9e14bdc37a6f8b9
Laura Messa Travin <laura.travin@gmail.com>
2019-04-10 20:29:52 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,36 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+
   end
+
+#create
+
+    #index
+    get '/articles' do
+        @articles= Article.all
+        erb :index
+    end
+    #new
+   get '/articles/new' do
+     erb :new
+   end
+   #new post
+   post '/articles' do
+    @new_article = Article.create(title: params[:title], content: params[:content])
+
+
+    # redirect to '/articles'
+     erb :index
+   end
+
+
+   get '/articles/:id' do
+     binding.pry
+     @article = Article.find(params[:id])
+     erb :show
+   end
+
+
+
 end
