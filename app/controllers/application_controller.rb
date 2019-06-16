ffff1e7c972089f8391ede6d7c669ba4f30f4354
Matthew Serre <matthew.serre@gmail.com>
2019-06-16 00:31:14 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,11 +27,11 @@ class ApplicationController < Sinatra::Base
   
   get '/articles/:id' do
     @article = Article.find(params[:id])
-    
     erb :show
   end
   
   get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
     erb :edit
   end
   
