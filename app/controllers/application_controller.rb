aa49ab2ab374e99cd553d9abb9503c2194842c79
Marcus Edwards 111 <Soulo@marcuss-mbp.attlocal.net>
2019-08-05 17:01:32 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,46 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to '/articles'
   end
+
+  get '/articles' do
+    @articles = Article.all
+    #binding.pry
+    erb :index
+  end
+
+  get '/articles/new' do
+    @article = Article.new
+    #binding.pry
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create(params)
+    #binding.pry
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article = Article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+    redirect to '/articles'
+  end
+
 end
