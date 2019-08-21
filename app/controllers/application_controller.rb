89969d12881e10063205eb140a5d531532f8a640
Diapause <diapause@protonmail.com>
2019-08-21 11:06:06 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,5 +8,49 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect '/articles'
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.new(params)
+    @article.save
+
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    if @article.update(params)
+      redirect "/articles/#{@article.id}"
+    else
+      render '/articles/:id/edit'
+    end
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.delete
+
+    redirect "/articles"
   end
 end
