889ea4dea5e3932c59e564f95a561b38991fedc8
Jonathan Fernandez <jfern20@gmail.com>
2020-02-05 22:32:11 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < Sinatra::Base
 
   post '/articles' do
     Article.create(params)
-    redirect '/posts'
+    redirect "/articles/#{id}"
   end
 
   get '/articles' do
