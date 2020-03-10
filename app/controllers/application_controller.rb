d8286c6b5b4154594337239bc6dac5bc207e590c
Niusha H <niusha.hadziomerovic@gmail.com>
2020-03-10 03:24:00 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 #  RUN rake db:migrate SINATRA_ENV=test
 
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -10,48 +9,51 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  # new
+  get "/articles/new" do
+    @article = Article.new
+    erb :new
   end
-  
-  get '/articles' do
+
+  get '/' do
+    redirect to "/articles"
+  end
+
+  # index
+  get "/articles" do
     @articles = Article.all
     erb :index
   end
-  
-  post '/articles' do
-    @article = Article.create(:title => params[:title], :content => params[:content])
-    redirect to "/articles/#{@article.id}"
+
+  # create
+  post "/articles" do
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
   end
 
-  get '/articles/new' do
-    erb :new
-  end
-  
-  get '/articles/:id' do
-    @article = Article.find_by_id(params[:id])
+  # show
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
     erb :show
   end
 
-  get '/articles/:id/edit' do  #load edit form
-    @article = Article.find_by_id(params[:id])
+  # edit
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
     erb :edit
   end
-   
-  patch '/articles/:id' do #edit action
-    @article = Article.find_by_id(params[:id])
-    @article.title = params[:title]
-    @article.content = params[:content]
-    @article.save
-    redirect to "/articles/#{@article.id}"
+
+  # update
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
   end
 
-  delete '/articles/:id' do 
-    @article = Article.find_by_id(params[:id])
-    @article.title = params[:title]
-    @article.content = params[:content]
-    @article.delete
-    @article.save
-    redirect to "/articles/"
+  #delete
+  delete '/articles/:id/delete' do
+    @article = Article.find(params["id"])
+    @article.destroy
+    erb :delete
   end
-
-end
+end 
