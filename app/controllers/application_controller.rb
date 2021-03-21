2bd5c3ec8d79c92686b08e7657024a42a5bebfbd
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-21 18:13:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,53 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :'../views/index'
+  end
+
+  get '/articles/new' do
+    erb :'../views/new'
+  end
+
+  post '/articles' do
+    @article = Article.new(title: params[:title], content: params[:content])
+    @article.save
+
+    redirect "/articles/#{@article[:id]}" # need double quotes to invoke string interpolation
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+
+    erb :'../views/show'
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+
+    erb :'../views/edit'
+  end
+
+  patch '/articles/:id' do
+
+    @article = Article.find(params[:id])
+
+    @article.title = params[:title]
+    @article.content = params[:content]
+
+    @article.save
+
+    redirect "/articles/#{@article[:id]}" # need double quotes to invoke string interpolation
+  end
+
+  delete '/articles/:id' do
+
+    Article.destroy(params[:id])
+
+    redirect "/articles"
+  end
+
 end
