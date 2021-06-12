1416d005dd13ebdbcabada1c5197fc479d516711
Daniel Suarez <suarez.daniel101@gmail.com>
2021-06-12 00:46:18 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,21 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-  end
-
   get '/articles' do
-    binding.pry
     @articles = Article.all
     erb :index
   end
+
+  get '/articles/:id' do
+    # binding.pry
+    @article = Article.find(params["id"])
+
+    erb :show
+  end
+
+  # get '/articles/new' do
+  #   # binding.pry
+  #   @articles = Article.all
+  #   erb :index
+  # end
 end
